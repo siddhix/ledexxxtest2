2fdf76759b30ffd08609f5d4620ad2703e3bc922
siddhix <41867661+siddhix@users.noreply.github.com>
2026-01-17 11:29:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 使用官方核心，避免 helloworld 的 gn 依赖
+rm -rf feeds/helloworld/{xray-core,gn,v2ray-core,sing-box}
